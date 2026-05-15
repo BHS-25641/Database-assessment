590f742f480ac1e61bcaf8c510e48bd80e3f1332
2564git config user.name Your <your@email.com>
2026-05-15 12:28:23 +1200

    added comments

--- dtp_internal.py
+++ dtp_internal.py
@@ -1,5 +1,16 @@
+#import sqilte3 into vs code
 import sqlite3
-db = sqlite3.connect("phonesgb.db")
-cursor = db.cursor()
 
+#variables to prevent magic numbers
+DATABASE = "phonesgb.db"
+
+
+
+
+db = sqlite3.connect(DATABASE)
+cursor = db.cursor()
+sql = "SELECT * from phonesgb;"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
 db.close()

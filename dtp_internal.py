c260fe9884fa500e1d292a742160282f56bad20c
2564git config user.name Your <your@email.com>
2026-05-18 14:33:30 +1200

    printed all results in a nicer way

--- dtp_internal.py
+++ dtp_internal.py
@@ -1,3 +1,4 @@
+#docstring phones databse application
 #import sqilte3 into vs code
 
 import sqlite3
@@ -8,15 +9,20 @@ DATABASE = "phonesgb.db"
 
 
 #functions
-
+def print_all_phonesgb():
+    '''print all the aircraft nicely'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * from phonesgb;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    for phones in results:
+        print(f"{phones[1]}{phones[2]}{phones[3]}{phones[4]}")
+    #loop finishes
+    db.close()
 
 
 
 #main code for db
-db = sqlite3.connect(DATABASE)
-cursor = db.cursor()
-sql = "SELECT * from phonesgb;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-db.close()
+print_all_phonesgb()

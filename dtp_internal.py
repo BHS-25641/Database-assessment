632e994d082629ea1e5384a6614d488e3ff9b0d7
2564git config user.name Your <your@email.com>
2026-05-19 12:42:22 +1200

    started menu interface

--- dtp_internal.py
+++ dtp_internal.py
@@ -1,14 +1,13 @@
-#docstring phones databse application
-#import sqilte3 into vs code
+# docstring phones databse application
+# import sqilte3 into vs code
 
 import sqlite3
 
-#variables to prevent magic numbers
+# variables to prevent magic numbers
 DATABASE = "phonesgb.db"
 
 
-
-#functions
+# functions
 def print_all_phonesgb():
     '''print all the aircraft nicely'''
     db = sqlite3.connect(DATABASE)
@@ -16,14 +15,18 @@ def print_all_phonesgb():
     sql = "SELECT * from phonesgb;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    #loop through all the results
-    print(f"name                     amount of gb   manufacturer     ranking ")
+    # loop through all the results
+    print("name                     amount of gb   manufacturer     ranking ")
     for phones in results:
         print(f"{phones[1]:<30}{phones[2]:<10}{phones[3]:<20}{phones[4]:<25}")
-    #loop finishes
+    # loop finishes
     db.close()
 
 
-
-#main code for db
-print_all_phonesgb()
+# web application menu
+While True:
+menu = print("""Welcome to my database application.
+             This is all about how much of gb within phones""")
+user_input = input("What would you like to see.\n1. Print all aircraft\n2.Exit\n3")
+if user_input == "1":
+    print_all_phonesgb()

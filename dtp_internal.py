6a8d3cf1f08a43df9ab47c27e1cce7babc23d051
2564git config user.name Your <your@email.com>
2026-05-22 09:01:37 +1200

    fixed functions and interface yay!

--- dtp_internal.py
+++ dtp_internal.py
@@ -27,49 +27,50 @@ def print_all_phonesgb():
 # function 2 - gb amount starting from highest
 def print_all_phonesgb_by_gb_desc():
     '''print all the phones sorted by highest gb '''
-db = sqlite3.connect(DATABASE)
-cursor = db.cursor()
-sql = "SELECT * from phonesgb ORDER BY gb_amount DESC;"
-cursor.execute(sql)
-results = cursor.fetchall()
-# loop through all the results
-print("name                     amount of gb   manufacturer     ranking ")
-for phones in results:
-    print(f"{phones[1]:<30}{phones[2]:<10}{phones[3]:<20}{phones[4]:<25}")
-# loop finishes
-db.close()
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * from phonesgb ORDER BY gb_amount DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(results)
+    # loop through all the results
+    print("name                     amount of gb   manufacturer     ranking ")
+    for phones in results:
+        print(f"{phones[1]:<30}{phones[2]:<10}{phones[3]:<20}{phones[4]:<25}")
+    # loop finishes
+    db.close()
 
 
 # function 3 - gb amount starting from lowest
 def print_all_phones_by_gb_asc():
     '''print all the phones sorted by lowest gb '''
-db = sqlite3.connect(DATABASE)
-cursor = db.cursor()
-sql = "SELECT * from phonesgb ORDER BY gb_amount;"
-cursor.execute(sql)
-results = cursor.fetchall()
-# loop through all the results
-print("name                     amount of gb   manufacturer     ranking ")
-for phones in results:
-    print(f"{phones[1]:<30}{phones[2]:<10}{phones[3]:<20}{phones[4]:<25}")
-# loop finishes
-db.close()
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * from phonesgb ORDER BY gb_amount;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # loop through all the results
+    print("name                     amount of gb   manufacturer     ranking ")
+    for phones in results:
+        print(f"{phones[1]:<30}{phones[2]:<10}{phones[3]:<20}{phones[4]:<25}")
+    # loop finishes
+    db.close()
 
 
 # function 4 - select all manufacturers 
 def print_all_phones_manufacturers():
     '''print all the phones by manufacturer '''
-db = sqlite3.connect(DATABASE)
-cursor = db.cursor()
-sql = "SELECT manufacturer from phonesgb;"
-cursor.execute(sql)
-results = cursor.fetchall()
-# loop through all the results
-print("name                     amount of gb   manufacturer     ranking ")
-for phones in results:
-    print(f"{phones[1]:<30}{phones[2]:<10}{phones[3]:<20}{phones[4]:<25}")
-# loop finishes
-db.close()
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT manufacturer from phonesgb;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # loop through all the results
+    print("manufacturer ")
+    for phones in results:
+        print(f"{phones[0]:<30}")
+    # loop finishes
+    db.close()
 
 
 # web application menu/ menu interface
@@ -84,7 +85,7 @@ while True:
         3. Print all phones by lowest gb
         4. Print all phone manufacturers
         5. Print all apple manufacturers
-        6. Print all samsuung manufacturers
+        6. Print all samsung manufacturers
         7. Exit
         """)
     if user_input == "1":
